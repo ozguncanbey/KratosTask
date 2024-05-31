10290035a44cdf2aef8d183694cba779fc06c7c9
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-05-31 09:05:13 +0300

    Initial Commit

---
+++ KratosTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KratosTask
+//
+//  Created by Özgün Can Beydili on 31.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
